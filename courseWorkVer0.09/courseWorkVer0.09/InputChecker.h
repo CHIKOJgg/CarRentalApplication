0d36ae60d637615bf2128a386197526d74756f81
Miraslau <miroslav131206@gmail.com>
2025-11-24 21:26:27 +0300

    Delete InputChecker.h

--- courseWorkVer0.09/courseWorkVer0.09/InputChecker.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-#include <string>
-
-namespace inputChecker {
-    bool isIntCorrect(const std::string& input);
-    bool isStringCorrect(const std::string& input);
-    bool isDoubleCorrect(const std::string& input);
-}
